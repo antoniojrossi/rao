a9c47e71a039dc9cdf8b0fbe1ef7ca9a662211f8
Antonio J Rossi <antoniojrossi@gmail.com>
2015-01-07 22:33:50 +0100

    Initial Commit

---
+++ RAO/RAO.h
@@ -0,0 +1,19 @@
+//
+//  RAO.h
+//  RAO
+//
+//  Created by Antonio J Rossi on 7/1/15.
+//  Copyright (c) 2015 Antonio J Rossi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RAO.
+FOUNDATION_EXPORT double RAOVersionNumber;
+
+//! Project version string for RAO.
+FOUNDATION_EXPORT const unsigned char RAOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RAO/PublicHeader.h>
+
+
